234d7e20240af2fdd7b00f0ee442a56e8d1f187b
DavidHalman <dvdhalman@gmail.com>
2018-10-29 16:32:43 -0700

    dth - add dropdown with list of device types

--- frontend/src/components/Stats1.js
+++ frontend/src/components/Stats1.js
@@ -2,10 +2,14 @@ import React, { Component } from 'react';
 //import axios from 'axios';
 import api from '../resources/Credentials';
 import * as axios from '../stubs/axios'
+import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 
 export default class Stats1 extends Component {
     state = {
+        chosenDeviceType: "",
+        deviceTypes: [],
         devices: [],
+        dropdownOpen: false,
         search: "",
     }
 
@@ -20,6 +24,55 @@ export default class Stats1 extends Component {
                 "Content-Type": "application/json"
             }
         }
+        axios.get(`https://devices.intouchhealth.com/api/v1/device_types`, config)
+        .then(res => {
+            this.setState(
+                {
+                    chosenDeviceType: res.device_types[0].name,
+                    deviceTypes: res.device_types,
+                }, () => {
+                    let config = {
+                        headers: {
+                            'crossDomain': true,
+                            "ITH-Username": api.username,
+                            "ITH-API-Key": api.key,
+                            "ITH-Device-Type": this.state.chosenDeviceType,
+                            "Content-Type": "application/json"
+                        }
+                    }
+                    axios.get(`https://devices.intouchhealth.com/api/v1/devices`, config)
+                    .then(res => {
+                        this.setState({
+                            devices: res.devices,
+                        })
+                    })
+                }
+            )
+        })
+
+    }
+
+    toggle = () => {
+        this.setState({
+            dropdownOpen: !this.state.dropdownOpen,
+        })
+    }
+
+    selectDeviceType = (deviceType) => {
+        this.setState({
+            chosenDeviceType: deviceType,
+        })
+
+        let config = {
+            headers: {
+                'crossDomain': true,
+                "ITH-Username": api.username,
+                "ITH-API-Key": api.key,
+                "ITH-Device-Type": deviceType,
+                "Content-Type": "application/json"
+            }
+        }
+        console.log(config)
         axios.get(`https://devices.intouchhealth.com/api/v1/devices`, config)
         .then(res => {
             this.setState({
@@ -31,6 +84,16 @@ export default class Stats1 extends Component {
     render(){
         return(
             <div>
+                <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+                    <DropdownToggle caret>
+                        Device Types
+                    </DropdownToggle>
+                    <DropdownMenu>
+                        {this.state.deviceTypes.map(deviceType => {
+                            return (<DropdownItem onClick={() => this.selectDeviceType(deviceType.name)}>{deviceType.name}</DropdownItem>)
+                        })}
+                    </DropdownMenu>
+                </Dropdown>
                 <input
                     type="text"
                     onChange={this.updateSearch}
