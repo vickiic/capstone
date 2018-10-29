b72bf69f44ffd50dc445e1933ef36125d47c1905
Calvin Wang <calvin.wang0628@gmail.com>
2018-10-29 16:27:05 -0700

    Init iOS App + Apple Watch App

---
+++ watchApp/watchApp/ViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  watchApp
+//
+//  Created by Calvin Wang on 10/29/18.
+//  Copyright © 2018 Calvin Wang. All rights reserved.
+//
+
+import UIKit
+import WatchConnectivity
+
+class ViewController: UIViewController, WCSessionDelegate  {
+
+  public func sessionDidDeactivate(_ session: WCSession) {
+    // Code
+  }
+  public func sessionDidBecomeInactive(_ session: WCSession) {
+    // Code
+  }
+  public func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+    // Dummy Implementation
+  }
+  
+  // if WCSession is supported then create a default session
+  fileprivate let session: WCSession? = WCSession.isSupported() ? WCSession.default : nil
+  
+  // activate the optional session?
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+    
+    session?.delegate = self
+    session?.activate()
+  }
+  
+  func session(_ session: WCSession, didReceiveMessage message: [String : Any], replyHandler: @escaping ([String : Any]) -> Void) {
+    
+    DispatchQueue.main.async {
+      print("Yes")
+      self.clickStatus.text = "Watch Click"
+    }
+  }
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  @IBOutlet weak var clickStatus: UILabel!
+  
+
+  @IBAction func click(_ sender: Any) {
+    self.clickStatus.text = "Phone Click"
+    print("clicked on Phone")
+  }
+}
+
