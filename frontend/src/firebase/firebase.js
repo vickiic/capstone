7452a2e70d0bcf12119ecc5a228f08ecb1a80191
henryjeng <32383983+henryjeng@users.noreply.github.com>
2018-10-31 02:34:56 -0700

    Update firebase.js

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/auth';
 
 const prodConfig = {
-    apiKey: YOUR_API_KEY,
-    authDomain: YOUR_AUTH_DOMAIN,
-    databaseURL: YOUR_DATABASE_URL,
-    projectId: YOUR_PROJECT_ID,
-    storageBucket: '',
-    messagingSenderId: YOUR_MESSAGING_SENDER_ID,
+    apiKey: "AIzaSyB6d0eWLz0Ury0nKI-Fs7cvlrTyZ8llMdI",
+    authDomain: "ithfrontend-d0aa2.firebaseapp.com",
+    databaseURL: "https://ithfrontend-d0aa2.firebaseio.com",
+    projectId: "ithfrontend-d0aa2",
+    storageBucket: "ithfrontend-d0aa2.appspot.com",
+    messagingSenderId: "513072734313",
   };
   
   const devConfig = {
