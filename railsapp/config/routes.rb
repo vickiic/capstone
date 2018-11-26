7f15779c92f43d40c922bde3313330625a52f334
DavidHalman <dvdhalman@gmail.com>
2018-11-25 16:17:34 -0800

    dth/vc - conversion mostly works

--- railsapp/config/routes.rb
+++ railsapp/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
+  get 'stats/index'
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/stats', to: 'stats#index'
 
   root 'welcome#index'
 end
