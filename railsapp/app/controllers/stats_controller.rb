1368b370529666007107e893791e131d366b8c38
DavidHalman <dvdhalman@gmail.com>
2018-12-07 23:31:44 -0800

    dth - added ability for each card to contain unique data for that person based on device_uid

--- railsapp/app/controllers/stats_controller.rb
+++ railsapp/app/controllers/stats_controller.rb
@@ -1,8 +1,9 @@
 class StatsController < ApplicationController
   def index
+    @patient = params[:id]
     api = JSON.parse File.read(Rails.root.join("app/assets/api.json"))
     _response = HTTParty.get(
-        'https://devices.intouchhealth.com/api/v1/static_field_values/heartRate?device_uid=ccb53ec5-cb64-4de4-bdd0-9bdc6e331683',
+        'https://devices.intouchhealth.com/api/v1/static_field_values/heartRate?device_uid=' + params[:id],
         headers: { 'ITH-API-Key': api["key"],
                    'ITH-Username': api["username"],
                    'Content-Type': 'application/json' }
--- railsapp/config/routes.rb
+++ railsapp/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get '/stats', to: 'stats#index'
+  get '/stats/:id', to: 'stats#index'
 
   root 'welcome#index'
 end
