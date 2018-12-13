6091964d56c5c1d985190eec5bf4d9e66b3012b6
DavidHalman <dvdhalman@gmail.com>
2018-12-12 19:15:54 -0800

    dth - shows if incorrect username/password

--- railsapp/app/controllers/login_controller.rb
+++ railsapp/app/controllers/login_controller.rb
@@ -1,13 +1,15 @@
 class LoginController < ApplicationController
   def index
+    @incorrect = if params[:incorrect] == nil then false else true end
+    puts @incorrect
   end
   def authenticate
     _login = params[:q]
     _password = params[:w]
 
     if _login == "admin"
-      redirect_to :controller => 'welcome', action: 'index' and return
+      redirect_to welcome_index_path(param_1: _login, param_2:_password) and return
     end
-    redirect_to :controller => 'login', :action => 'index'
+    redirect_to login_index_path(incorrect: true)
   end
 end
