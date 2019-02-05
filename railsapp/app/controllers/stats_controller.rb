4e6b845aa2b310fc84e6b4243576dc15d8a274fc
DavidHalman <dvdhalman@gmail.com>
2019-02-05 15:20:26 -0800

    dth - fixed date form fields

--- railsapp/app/controllers/stats_controller.rb
+++ railsapp/app/controllers/stats_controller.rb
@@ -5,73 +5,50 @@ class StatsController < ApplicationController
       #if one just goes to the stats page, just show one of the patients. Helps with tests as well.
       @patient = "12345abc"
     end
-    @startYear = params[:startYear]
-    @startMonth = params[:startMonth]
-    @startDay = params[:startDay]
-    @endYear = params[:endYear]
-    @endMonth = params[:endMonth]
-    @endDay = params[:endDay]
+    _startYear = params[:startYear]
+    _startMonth = params[:startMonth]
+    _startDay = params[:startDay]
+    _endYear = params[:endYear]
+    _endMonth = params[:endMonth]
+    _endDay = params[:endDay]
 
     # _api = JSON.parse File.read(Rails.root.join("app/assets/api.json"))
     if (params[:commit] == nil) then
-    #   _response = HTTParty.get(
-    #       'https://devices.intouchhealth.com/api/v1/static_field_values/heartRate?device_uid=' + @patient + '&limit=1000',
-    #       headers: { 'ITH-API-Key': _api["key"],
-    #                  'ITH-Username': _api["username"],
-    #                  'Content-Type': 'application/json' }
-    #   )
+      @heartrates = Heartrate.where("device = ?", @patient)
     else
       #check for nil
-      if @startYear == '' then
-        @startYear = '0000'
+      if _startYear == '' || _startYear == nil then
+        _startYear = '0'
       end
-      if @startMonth == '' then
-        @startMonth = '01'
+      if _startMonth == '' || _startMonth == nil then
+        _startMonth = '1'
       end
-      if @startDay == '' then
-        @startDay = '01'
+      if _startDay == '' || _startDay == nil then
+        _startDay = '1'
       end
-      if @endYear == '' || @endYear == nil then
-        @endYear = Time.now.strftime("%Y")
+      if _endYear == '' || _endYear == nil then
+        _endYear = Time.now.strftime("%Y")
       end
-      if @endMonth == '' || @endMonth == nil then
-        @endMonth = Time.now.strftime("%m")
+      if _endMonth == '' || _endMonth == nil then
+        _endMonth = Time.now.strftime("%m")
       end
-      if @endDay == '' || @endDay == nil then
-        @endDay = Time.now.strftime("%d")
-      end
-
-      # puts "Start Year: " + @startYear
-      # puts "Start Month: " + @startMonth
-      # puts "Start Day: " + @startDay
-      # puts "End Year: " + @endYear
-      # puts "End Month: " + @endMonth
-      # puts "End Day: " + @endDay
-
-
-      #add preceding '0' to day fields if single digits
-      if @startDay.length == 1 then
-        @startDay = '0' + @startDay
-      end
-      if @endDay.length == 1 then
-        @endDay = '0' + @endDay
-      end
-      if @startMonth.length == 1 then
-        @startMonth = '0' + @startMonth
-      end
-      if @endMonth.length == 1 then
-        @endMonth = '0' + @endMonth
+      if _endDay == '' || _endDay == nil then
+        _endDay = Time.now.strftime("%d")
       end
 
-      # _response = HTTParty.get(
-      #     'https://devices.intouchhealth.com/api/v1/static_field_values/heartRate?device_uid=' + @patient + '&datetime_begin=' + @startYear + '-' + @startMonth + '-' + @startDay + 'T00:00:00-07:00&datetime_end=' + @endYear + '-' + @endMonth + '-' + @endDay + 'T23:41:37-07:00&limit=1000',
-      #     headers: { 'ITH-API-Key': _api["key"],
-      #                'ITH-Username': _api["username"],
-      #                'Content-Type': 'application/json' }
-      # )
+      # puts "Start Year: " + _startYear
+      # puts "Start Month: " + _startMonth
+      # puts "Start Day: " + _startDay
+      # puts "End Year: " + _endYear
+      # puts "End Month: " + _endMonth
+      # puts "End Day: " + _endDay
+
+      _startDate = Date.new(_startYear.to_i, _startMonth.to_i, _startDay.to_i)
+      _endDate = Date.new(_endYear.to_i, _endMonth.to_i, _endDay.to_i)
+      puts _startDate
+      puts _endDate
+      # @heartrates = Heartrate.where("device = ?", @patient)
+      @heartrates = Heartrate.where("device = ? AND created_at > ? AND created_at < ?", @patient, _startDate, _endDate)
     end
-    # @body = JSON.parse(_response.body)
-    @heartrates = Heartrate.where("device = ?", @patient)
-    # @heartrates = Heartrate.all.where(:device, @patient)
   end
 end
