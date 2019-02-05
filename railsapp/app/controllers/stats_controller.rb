f8bd1e7ff12e7635bdef7aba41d9763a82213c90
DavidHalman <dvdhalman@gmail.com>
2019-02-05 15:29:04 -0800

    dth - removed comments and print statements

--- railsapp/app/controllers/stats_controller.rb
+++ railsapp/app/controllers/stats_controller.rb
@@ -12,7 +12,6 @@ class StatsController < ApplicationController
     _endMonth = params[:endMonth]
     _endDay = params[:endDay]
 
-    # _api = JSON.parse File.read(Rails.root.join("app/assets/api.json"))
     if (params[:commit] == nil) then
       @heartrates = Heartrate.where("device = ?", @patient)
     else
@@ -36,18 +35,8 @@ class StatsController < ApplicationController
         _endDay = Time.now.strftime("%d")
       end
 
-      # puts "Start Year: " + _startYear
-      # puts "Start Month: " + _startMonth
-      # puts "Start Day: " + _startDay
-      # puts "End Year: " + _endYear
-      # puts "End Month: " + _endMonth
-      # puts "End Day: " + _endDay
-
       _startDate = Date.new(_startYear.to_i, _startMonth.to_i, _startDay.to_i)
       _endDate = Date.new(_endYear.to_i, _endMonth.to_i, _endDay.to_i)
-      puts _startDate
-      puts _endDate
-      # @heartrates = Heartrate.where("device = ?", @patient)
       @heartrates = Heartrate.where("device = ? AND created_at > ? AND created_at < ?", @patient, _startDate, _endDate)
     end
   end
