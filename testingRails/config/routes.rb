e6edf061a01aba79d7c8ec70ef2143044101f0e8
DavidHalman <dvdhalman@gmail.com>
2019-03-04 18:55:40 -0800

    dth -update the gcloud credentials

---
+++ testingRails/config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  resources :cats
+  get 'welcome/index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index'
+end
